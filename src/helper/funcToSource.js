42d9535c0a48f205b7ab40ca559532b5dcc4da4c
Josh Ouyang <me@joshoy.org>
2022-09-19 23:58:44 +0800

    fix: funcToSource helper function

--- src/helper/funcToSource.js
+++ src/helper/funcToSource.js
@@ -1,14 +1,15 @@
 export function funcToSource(fn, sourcemapArg) {
     var sourcemap = sourcemapArg === undefined ? null : sourcemapArg;
     var source = fn.toString();
-    var lines = source.split('\n');
-    lines.pop();
-    lines.shift();
-    var blankPrefixLength = lines[0].search(/\S/);
     var regex = /(['"])__worker_loader_strict__(['"])/g;
-    for (var i = 0, n = lines.length; i < n; ++i) {
-        lines[i] = lines[i].substring(blankPrefixLength).replace(regex, '$1use strict$2') + '\n';
-    }
+    var lines = [];
+
+    // Ref: https://github.com/Kitware/vtk-js/pull/2361/commits/16a07df4116d1409d0650139c21badd744de1402
+    // instead of extracting the function source code, just return the function as if it's being evaluated
+    // by the caller.
+    source = source.replace(regex, '$1use strict$2');
+    lines.push('(' + source + ')()');
+
     if (sourcemap) {
         lines.push('\/\/# sourceMappingURL=' + sourcemap + '\n');
     }
